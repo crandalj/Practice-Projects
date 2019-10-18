43cae72c659699c4fae9d681ee641fefdc87ed74
Joseph Crandal <crandalj@gmail.com>
2019-10-18 16:06:28 -0400

    color tweaks & cont. function dev
    tweaked colors for income and expense to have a prettier shade and font color, also started adding the retrieval of input in the code

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,17 +1,53 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
-    // Code
+    var incomeList = [];
+    var expenseList = [];
+    var totalValue = 0;
+
+    var budgetAddItem = function(item){
+        console.log(item.type);
+        console.log(item.desc);
+        console.log(item.value);
+    };
 })();
 
 // UI CONTROLLER
 var UIController = (function(){
-    // Code
+    return {
+        getInput: function(){
+            return{
+                type: document.querySelector('.inputType').value,
+                desc: document.querySelector('.inputDesc').textContent,
+                value: document.querySelector('.inputValue').value
+            }
+        }
+    }
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    document.querySelector('.add_btn').addEventListener('click', function(){
+
+    var ctrlAddItem = function() {
         console.log("Button was clicked!");
+
+        // Get input data
+        var data = UICtrl.getInput();
+        
+        // add item to budget controller
+        
+        budgetCtrl.budgetAddItem(data);
+        // add item to UI
+
+        // calculate new budget total
+
+        // display budget on UI
+    };
+    document.querySelector('.btn_add').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
     });
 
 })(budgetController, UIController);
