6733413bbf78abb14276d75a445d98ab3c13a9c0
Joseph Crandal <crandalj@gmail.com>
2019-10-18 22:52:28 -0400

    budget math linked up
    Can add expenses and income and they get updated in the UI appropriately. Keeps two decimals as well.

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,24 +1,48 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
-    var incomeList = [];
-    var expenseList = [];
-    var totalValue = 0;
+    var income = 0.00;
+    var expense = 0.00;
+    var budget = 0.00;
 
-    var budgetAddItem = function(item){
-        console.log(item.type);
-        console.log(item.desc);
-        console.log(item.value);
-    };
+    return{
+        budgetAddItem: function(item){
+            if(item.type == "+"){
+                income = parseFloat(Math.round((parseFloat(income) + parseFloat(item.value)) * 100) / 100).toFixed(2);
+            } else {
+                expense = parseFloat(Math.round((expense - item.value) * 100)/100).toFixed(2);
+            }
+            budget = parseFloat(Math.round((parseFloat(income) + parseFloat(expense))*100)/100).toFixed(2);
+        },
+        getBudget: function(){
+            return{
+                income: income,
+                expense: expense,
+                budget: budget
+            }
+        }
+    }
 })();
 
 // UI CONTROLLER
 var UIController = (function(){
+    var incomeUI = document.querySelector('.incomeValue');
+    var expenseUI = document.querySelector('.expensesValue');
+    var budgetUI = document.querySelector('.budgetTotal');
     return {
         getInput: function(){
             return{
                 type: document.querySelector('.inputType').value,
                 desc: document.querySelector('.inputDesc').textContent,
                 value: document.querySelector('.inputValue').value
+            }
+        },
+        updateBudget: function(data){
+            incomeUI.textContent = "+ " + data.income;
+            expenseUI.textContent = "- " + Math.abs(data.expense);
+            if(data.budget >= 0){
+                budgetUI.textContent = "+ " + data.budget;
+            } else {
+                budgetUI.textContent = "- " + Math.abs(data.budget);
             }
         }
     }
@@ -34,9 +58,10 @@ var controller = (function(budgetCtrl, UICtrl){
         var data = UICtrl.getInput();
         
         // add item to budget controller
-        
         budgetCtrl.budgetAddItem(data);
+
         // add item to UI
+        UICtrl.updateBudget(budgetCtrl.getBudget());
 
         // calculate new budget total
 
