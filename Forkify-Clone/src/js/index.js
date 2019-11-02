12d183cac644b1c14d6a3e739247754572491508
Joseph Crandal <crandalj@gmail.com>
2019-11-01 21:42:24 -0400

    Update index.js

--- Forkify-Clone/src/js/index.js
+++ Forkify-Clone/src/js/index.js
@@ -1,5 +1,32 @@
 // Global app controller
 import Search from './models/Search';
 
-const search = new Search('pizza');
-search.getResults();
+// Global state of app
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked recipes
+const state = {};
+
+const controlSearch = async () => {
+    // get query from view
+    const query = 'pizza' //TODO
+    if (query){
+        // new search object and add to state
+        state.search = new Search(query);
+
+        // prepare UI for results
+
+        // search for recipes
+        await state.search.getResults();
+
+        // render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e=>{
+    e.preventDefault();
+    controlSearch();
+});
+
